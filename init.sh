4310f5dcb41a5949fa39422f8074383b8c4633b8
takzhanov <takzhanov+github@gmail.com>
2016-11-15 17:50:55 +0300

    fix. pass lesson 2

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-#sudo /etc/init.d/nginx restart
-sudo nginx -s reload
+sudo /etc/init.d/nginx restart
+#sudo nginx -s reload
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 nohup gunicorn -b 0.0.0.0:8080 hello:app &
