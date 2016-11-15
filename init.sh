52338eb0a7a4c5c76d3920f4dac45e219ac742a8
takzhanov <takzhanov+github@gmail.com>
2016-11-15 18:24:44 +0300

    refactoring

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+#local
+#ln -sf ~/dev/dev/stepic-web-tech /home/box/web
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo nginx -s reload
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
-nohup gunicorn -b 0.0.0.0:8080 hello:app &
+sudo mkdir -p /etc/gunicorn.d
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello
+sudo /etc/init.d/gunicorn restart
+#nohup gunicorn -b 0.0.0.0:8080 hello:app &
 #sudo /etc/init.d/mysql start
