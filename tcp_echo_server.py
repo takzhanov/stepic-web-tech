ddfefe351ca0bd04679975611e7d4d624c804b5e
takzhanov <takzhanov+github@gmail.com>
2016-11-19 00:17:39 +0300

    tcp_echo_server v2

--- tcp_echo_server.py
+++ tcp_echo_server.py
@@ -3,19 +3,19 @@
 
 import socket
 
-sock = socket.socket()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('0.0.0.0', 2222))
-sock.listen(1)
-conn, addr = sock.accept()
+sock.listen(10)
 
-print('connected:', addr)
 
 while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    if data == 'close':
-        break
-    conn.send(data)
-
-conn.close()
+    conn, addr = sock.accept()
+    print('connected:', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        if data == 'close':
+            break
+        conn.send(data)
+    conn.close()
