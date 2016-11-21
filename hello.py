885cd80af24c1173e1a16ae0ef2d9e5b288392ca
takzhanov <takzhanov+github@gmail.com>
2016-11-21 10:47:35 +0300

    fix

--- hello.py
+++ hello.py
@@ -2,10 +2,10 @@ def app(environ, start_response):
     s = ""
 
     for i in environ['QUERY_STRING'].split("&"):
-        s = s + i + "\n"
+        s = s + i + "\r\n"
 
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(s)))
     ])
-    return iter([s])
+    return [bytes(s, 'utf-8')]
