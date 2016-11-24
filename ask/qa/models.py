f96e9bb171bb4de9b2c5debf793125e088bcb58b
takzhanov <takzhanov+github@gmail.com>
2016-11-24 21:00:57 +0300

    fix models and script

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import Model, Count
+from django.utils import timezone
 
 
 class Question(Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField()
-    rating = models.IntegerField()
+    added_at = models.DateTimeField(default=timezone.now())
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, related_name='author')
     likes = models.ManyToManyField(User)
 
