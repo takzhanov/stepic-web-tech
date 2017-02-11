aa18748c8b8c4594934ea8418b49cef17fa39b43
Yury Takzhanov <takzhanov@restream.rt.ru>
2017-02-11 23:18:44 +0300

    Task 2.6.11 Pass

--- prep-env.sh
+++ prep-env.sh
@@ -6,5 +6,7 @@ sudo pip3 install django gunicorn==17.5
 #sudo sed -i 's/bin\/python/bin\/python3/g' /usr/bin/gunicorn*
 #sudo sed -i 's/17.5/19.6/g' /usr/bin/gunicorn*
 
+sudo chmod -R a+rw . #особенности проверяющей платформы, про доступ к базе видимо
+
 sed -i 's/DEBUG = True/DEBUG = False/g' /home/box/web/ask/ask/settings.py
 sed -i 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['\''\*'\''\]/g' /home/box/web/ask/ask/settings.py
