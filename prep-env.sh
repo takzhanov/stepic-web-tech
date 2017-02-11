017d2f1d8c33fd9e10f705798952086c558c75ca
Yury Takzhanov <takzhanov@restream.rt.ru>
2017-02-11 21:17:27 +0300

    упрощение подготовки серверного окружения

--- prep-env.sh
+++ prep-env.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-sudo pip3 install django gunicorn
+sudo pip3 install django gunicorn==17.5
 # поправить файлы gunicorn https://github.com/fedorch/stepic_web_project/blob/master/python3.md
-sudo sed -i 's/bin\/python/bin\/python3/g' /usr/sbin/gunicorn*
-sudo sed -i 's/bin\/python/bin\/python3/g' /usr/bin/gunicorn*
-sudo sed -i 's/17.5/19.6/g' /usr/bin/gunicorn*
+#sudo sed -i 's/bin\/python/bin\/python3/g' /usr/sbin/gunicorn*
+#sudo sed -i 's/bin\/python/bin\/python3/g' /usr/bin/gunicorn*
+#sudo sed -i 's/17.5/19.6/g' /usr/bin/gunicorn*
 
 sed -i 's/DEBUG = True/DEBUG = False/g' /home/box/web/ask/ask/settings.py
 sed -i 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['\''\*'\''\]/g' /home/box/web/ask/ask/settings.py
